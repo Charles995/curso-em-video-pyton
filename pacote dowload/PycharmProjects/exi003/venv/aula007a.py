aca67bb58ab60fa4b3b6dea4c1d3cfaafe97fb4a
Charles995 <Charles95777@gmail.com>
2021-03-01 17:32:47 -0300

    Charles  exercícios (python)
    exercícios p

---
+++ pacote dowload/PycharmProjects/exi003/venv/aula007a.py
@@ -0,0 +1,6 @@
+a=str(input('qual o seu nome?'))
+nasci=int(input('Em que anos você nasceu?'))
+
+idade=(2021-nasci)
+print('Seja bem vindo,{}!!!!!!!!!!!!!!!!!!!!!!!!!!'.format(a), end='')
+print('  Você tem {} anos de idade .'.format(idade))
